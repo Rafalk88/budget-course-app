d33f1909e4d9ef21723c344701c9c73fa1f561af
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-06-20 12:06:09 +0200

    chore: install formatter & rules

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { Helmet } from 'react-helmet';
-import React from 'react'
-import './App.css';
+import { Helmet } from "react-helmet";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Helmet>
         <title>Budget app - main</title>
         <meta name="description" content="Strona główna aplikacji" />
       </Helmet>
       <header className="App">
-        <div className="App-header">
-          Initialize project
-        </div>
+        <div className="App-header">Initialize project</div>
       </header>
-    </React.Fragment>
+    </>
   );
 }
 
