52ce811fdbba4ad560ae7ae3d18b86aaf2ebc0ce
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-07-02 01:33:57 +0200

    CSS: change padding

--- src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.css.js
+++ src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.css.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Category = styled.div`
   border: 1px solid ${({ theme: { colors } }) => colors.gray.dark};
-  padding: ${({ theme: { spacing } }) => spacing.xs}px;
+  padding: ${({ theme: { spacing } }) => spacing.sm}px;
   display: flex;
   justify-content: space-between;
   cursor: pointer;
