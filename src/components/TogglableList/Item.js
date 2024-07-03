9ebc8590e8db3bb73ba24769b3893fb1848eb438
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-07-03 22:50:48 +0200

    refa: split two components

---
+++ src/components/TogglableList/Item.js
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { selectParentCategory as selectParentCategoryAction } from 'data/actions/budget.actions';
+
+function Component({ item, onClickHandler, isActive, selectParentCategory }) {
+  const handleClick = () => {
+    if (isActive) {
+      onClickHandler(null);
+      selectParentCategory(null);
+    } else {
+      onClickHandler(item.id);
+      selectParentCategory(item.id);
+    }
+  };
+
+  return (
+    <div>
+      <item.Trigger onClick={handleClick} />
+      {isActive && item.children}
+    </div>
+  );
+}
+
+const mapDispatchToProps = {
+  selectParentCategory: selectParentCategoryAction,
+};
+
+export const Item = connect(null, mapDispatchToProps)(Component);
+
+Component.propTypes = {
+  item: PropTypes.shape({}).isRequired,
+  onClickHandler: PropTypes.func.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  selectParentCategory: PropTypes.func.isRequired,
+};
--- src/components/TogglableList/TogglableList.js
+++ src/components/TogglableList/TogglableList.js
@@ -1,28 +1,7 @@
-/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { selectParentCategory as selectParentCategoryAction } from 'data/actions/budget.actions';
-
-function Component({ item, onClickHandler, isActive, selectParentCategory }) {
-  const handleClick = () => {
-    if (isActive) {
-      onClickHandler(null);
-      selectParentCategory(null);
-    } else {
-      onClickHandler(item.id);
-      selectParentCategory(item.id);
-    }
-  };
-
-  return (
-    <div>
-      <item.Trigger onClick={handleClick} />
-      {isActive && item.children}
-    </div>
-  );
-}
+import { Item } from './Item';
 
 export function TogglableList({ items }) {
   const [selectedItem, setSelectedItem] = useState(null);
@@ -40,19 +19,6 @@ export function TogglableList({ items }) {
     </>
   );
 }
-
-const mapDispatchToProps = {
-  selectParentCategory: selectParentCategoryAction,
-};
-
-export const Item = connect(null, mapDispatchToProps)(Component);
-
-Component.propTypes = {
-  item: PropTypes.shape({}).isRequired,
-  onClickHandler: PropTypes.func.isRequired,
-  isActive: PropTypes.bool.isRequired,
-  selectParentCategory: PropTypes.func.isRequired,
-};
 
 TogglableList.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape([])).isRequired,
