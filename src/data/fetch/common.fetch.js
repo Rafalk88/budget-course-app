82cd93f7238e4d0d802803cde7a41dab4ca0fe48
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-09-22 20:03:22 +0200

    fix: add parentCategory not empty array

--- src/data/fetch/common.fetch.js
+++ src/data/fetch/common.fetch.js
@@ -1,6 +1,6 @@
 export const fetchAllCategories = async () => {
   const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/categories/?_embed=parentCategory`,
+    `${process.env.REACT_APP_API_URL}/categories/?_expand=parentCategory`,
   );
   const data = await response.json();
 
