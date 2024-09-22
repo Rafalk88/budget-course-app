b4b6d330c4e927ca4a25346b5aa2fbd4a0dda5b8
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-09-22 21:59:30 +0200

    refa&chore: add useMutation & update versionm

--- src/pages/Budget/components/AddTransactionForm/AddTransactionView.js
+++ src/pages/Budget/components/AddTransactionForm/AddTransactionView.js
@@ -1,15 +1,11 @@
-import React, { useCallback } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import { budget as APIBudget, common as APICommon } from 'data/fetch';
-import PropTypes from 'prop-types';
-
-import { addTransaction } from 'data/actions/budget.actions';
 
 import { AddTransactionForm } from './AddTransactionForm';
 
-export function Component({ dispatchAddTransaction }) {
+export function AddTransactionView() {
   const { data: budget } = useQuery({
     queryKey: ['budget'],
     queryFn: () => APIBudget.fetchBudget({ id: 1 }),
@@ -18,23 +14,23 @@ export function Component({ dispatchAddTransaction }) {
     queryKey: ['allCategories'],
     queryFn: APICommon.fetchAllCategories,
   });
+  const { mutate } = useMutation({ mutationFn: APIBudget.addTransaction });
   const navigate = useNavigate();
 
-  const handleSubmitAddTransaction = useCallback(
-    (values) => {
-      const valuesWithDate = {
-        ...values,
-        date: new Date(),
-        budgetId: budget.id,
-      };
+  const handleSubmitAddTransaction = (values) => {
+    const valuesWithDate = {
+      ...values,
+      date: new Date(),
+      budgetId: budget.id,
+    };
 
-      dispatchAddTransaction({
-        data: valuesWithDate,
-        successMessage: 'Transaction has been added!',
-      }).then(() => navigate(-1));
-    },
-    [budget, allCategories, dispatchAddTransaction, navigate],
-  );
+    mutate(
+      { budgetId: budget.id, data: valuesWithDate },
+      {
+        onSuccess: () => navigate(-1),
+      },
+    );
+  };
 
   return (
     <AddTransactionForm
@@ -44,13 +40,3 @@ export function Component({ dispatchAddTransaction }) {
     />
   );
 }
-
-const mapDispatchToProps = {
-  dispatchAddTransaction: addTransaction,
-};
-
-Component.propTypes = {
-  dispatchAddTransaction: PropTypes.func.isRequired,
-};
-
-export const AddTransactionView = connect(null, mapDispatchToProps)(Component);
