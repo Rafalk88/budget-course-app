5264bcf211c9dba5b7479ae4ca5bb860a91446bd
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-09-22 21:35:53 +0200

    fix: up-to-date transactions data

--- db-middleware.js
+++ db-middleware.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  res.header('Cache-Control', 'public, max-age=31536000');
-  res.removeHeader('pragma');
+  // res.header('Cache-Control', 'public, max-age=31536000');
+  // res.removeHeader('pragma');
   next();
 };
