44da0acb25c4c69ce9e486588fbe08b3ab0235d1
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-09-23 23:26:57 +0200

    feat: delete redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import configureStore from 'data/store';
 import App from './App';
 import './i18n/i18n';
-
-const store = configureStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
 );
